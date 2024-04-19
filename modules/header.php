cd4052f3c85844ff4b9a53c0abcf4de82be08237
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-04-19 16:40:09 -0700

    Remove unused headers

--- modules/header.php
+++ modules/header.php
@@ -149,11 +149,6 @@ if ($loggedIn) {
         }
     } 
 	
-//Added 4/13/2022. Intended to help with image uploads. May slow everything else down.	
-header("Cache-Control: no-cache, must-revalidate"); // HTTP 1.1
-header("Pragma: no-cache"); // HTTP 1.0
-header("Expires: Wed, 1 Jan 2020 00:00:00 GMT"); // Anytime in the past
-	
 	?>
 
 </head>
