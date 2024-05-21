2650c41360878b64f50068a3489d0f4b82a6623b
radiantBear <85893453+radiantBear@users.noreply.github.com>
2024-05-21 11:42:54 -0700

    Make permissions updates faster

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # This script enforces some rules before changes are committed. For now, it just
 # sets the file permissions for all files and directories so they are accessible
@@ -20,14 +20,36 @@ do
     FILE_PERMISSIONS=$(stat -c "%a" "$f")
 
     if [ -d "$f" ] && [ "$FILE_PERMISSIONS" != '2775' ]; then
+        echo
         echo "ERROR: Found directory '$f' with incorrect permissions '$FILE_PERMISSIONS'"
-        echo "Run 'chmod 775 \"$f\"' (or 'sh scripts/allow.sh' to fix all permissions) from the repository root before committing."
-        exit 1
+        echo "Would you like to update this directory's permissions?"
+        read -p "y/n: " yn < /dev/tty
+
+        if [ "$yn" = "y" ] || [ "$yn" = "yes" ]; then
+            chmod 775 "$f"
+        else
+            echo "Run 'chmod 775 \"$f\"' (or 'sh scripts/allow.sh' to fix all permissions) from the repository root before committing."
+            echo "Update 2024: scripts/allow.sh is out of date. Must be updated before being used."
+            exit 1
+        fi
+
     elif [ -f "$f" ] && [ "$FILE_PERMISSIONS" != '775' ]; then
+        echo
         echo "ERROR: Found file '$f' with incorrect permissions '$FILE_PERMISSIONS'"
-        echo "Run 'chmod 775 \"$f\"' (or 'sh scripts/allow.sh' to fix all permissions) from the repository root before committing."
-        exit 1
+        echo "Would you like to update this file's permissions to rwxrwxr-x?"
+        read -p "y/n: " yn < /dev/tty
+
+        if [ "$yn" = "y" ]; then
+            chmod 775 "$f"
+        else
+            echo "Run 'chmod 775 \"$f\"' (or 'sh scripts/allow.sh' to fix all permissions) from the repository root before committing."
+            echo "Update 2024: scripts/allow.sh is out of date. Must be updated before being used."
+            exit 1
+        fi
     fi
 done
 
+# Used to propagate the exit status from the while loop for the whole script
+status=$?
 echo "Pre-commit done."
+exit $status
