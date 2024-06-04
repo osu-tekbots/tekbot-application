c78a49a985fb0ee355ddc6fb615f268d4b372410
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2024-06-04 16:41:36 -0700

    Add link to feedback form

--- modules/footer.php
+++ modules/footer.php
@@ -1,9 +1,10 @@
-<?php 
-/**
- * Placeholder for future implementation
- */
-?>
-</main>
-<footer></footer>
+    </main>
+    <footer>
+        <!-- ?entry.1671399415=TekBots specifies checkbox to select for question 1 -->
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSdK6-dYdAUel_5yGWeJWiO7ptoXFscGZzRHhRI4FY7I1BDRog/viewform?entry.1671399415=TekBots" target="_blank" class="btn" type="button" style="position: fixed; right: 20px; bottom: 20px; z-index: 1001; background: rgb(195, 69, 0); color: #fff; font-size: 1rem; padding: .375rem .75rem; border-radius: 0.25rem; font-weight: normal;">
+            <i class="far fa-comment mr-1"></i>
+            Feedback
+        </a>
+    </footer>
 </body>
 </html>
